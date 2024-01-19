36480fb1983aab70e9f05df5c2cad790ff828a5b
Madison <madie-v@live.com>
2024-01-18 19:05:55 -0600

    added all index.js and my connection.js

---
+++ config/connecton.js
@@ -0,0 +1,12 @@
+// connection.js
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error!</h1>');
+  });
+
+router.use('/api', apiRoutes);
+
+module.exports = router;
